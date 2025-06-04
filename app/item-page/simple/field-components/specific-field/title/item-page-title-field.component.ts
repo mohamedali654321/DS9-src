55ce351baf905990a1cb6385aea17f6d7812621b
Mohamed Ali <66433220+mohamedali654321@users.noreply.github.com>
2025-06-04 11:43:56 -0700

    first commit

--- app/item-page/simple/field-components/specific-field/title/item-page-title-field.component.ts
+++ app/item-page/simple/field-components/specific-field/title/item-page-title-field.component.ts
@@ -7,6 +7,8 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { DSONameService } from '../../../../../core/breadcrumbs/dso-name.service';
 import { Item } from '../../../../../core/shared/item.model';
+import { KwareTranslatePipe } from "../../../../../shared/utils/kware-translate.pipe";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'ds-base-item-page-title-field',
@@ -14,7 +16,9 @@ import { Item } from '../../../../../core/shared/item.model';
   standalone: true,
   imports: [
     TranslateModule,
-  ],
+    KwareTranslatePipe,
+    NgIf
+],
 })
 /**
  * This component is used for displaying the title (defined by the {@link DSONameService}) of an item
--- themes/custom/app/item-page/simple/field-components/specific-field/title/item-page-title-field.component.ts
+++ themes/custom/app/item-page/simple/field-components/specific-field/title/item-page-title-field.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { ItemPageTitleFieldComponent as BaseComponent } from '../../../../../../../../app/item-page/simple/field-components/specific-field/title/item-page-title-field.component';
+import { KwareTranslatePipe } from 'src/app/shared/utils/kware-translate.pipe';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'ds-themed-item-page-title-field',
@@ -10,6 +12,8 @@ import { ItemPageTitleFieldComponent as BaseComponent } from '../../../../../../
   standalone: true,
   imports: [
     TranslateModule,
+    KwareTranslatePipe,
+    NgIf
   ],
 })
 export class ItemPageTitleFieldComponent extends BaseComponent {
